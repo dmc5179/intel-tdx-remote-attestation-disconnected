3ece47c26245fd3db3819aad94c0f02aba9e16ed
Dan Clark <danclark@redhat.com>
2026-05-22 15:45:41 -0400

    Initial commit

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-podman build --squash -t quay.io/intel-tdx/pcs-base:latest -f PCS-Base-Containerfile .
+AUTHFILE=/home/danclark/quay-pull-secret.json
 
-podman build --squash -t quay.io/intel-tdx/pcs-client-tool:latest -f PCS-Client-Tool-Containerfile .
+podman build --squash -t quay.io/danclark/intel-tdx/pcs-base:latest -f PCS-Base-Containerfile .
 
-#podman build -t -f PCCS-Admin-Tool-Containerfile .
+podman push --authfile=${AUTHFILE} quay.io/danclark/intel-tdx/pcs-base:latest
 
-#podman build -t -f PCKCIDRT-Containerfile .
+podman build --squash -t quay.io/danclark/intel-tdx/pcs-client-tool:latest -f PCS-Client-Tool-Containerfile .
+
+podman push --authfile=${AUTHFILE} quay.io/danclark/intel-tdx/pcs-client-tool:latest
+
+podman build --squash -t quay.io/danclark/intel-tdx/pccs-admin-tool:latest  -f PCCS-Admin-Tool-Containerfile .
+
+podman push --authfile=${AUTHFILE} quay.io/danclark/intel-tdx/pccs-admin-tool:latest
+
+#podman build --squash -t quay.io/danclark/intel-tdx/pckcidrt:latest -f PCKCIDRT-Containerfile .
+
+#podman push --authfile=${AUTHFILE} quay.io/danclark/intel-tdx/pckcidrt:latest
